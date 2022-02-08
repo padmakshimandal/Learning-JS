b8ff5a7bf8fccc8cb451f4d9880048682bab6817
padmakshimandal <84122297+padmakshimandal@users.noreply.github.com>
2022-02-08 22:32:13 +0530

    Create Function.js

---
+++ Function.js
@@ -0,0 +1,7 @@
+function cookMaagi(maagi,water,pot){
+    console.log("Your maagi will be ready in "
+                +maagi*2+" minutes"
+                + " using "+ water+" cups of water"
+                +" using "+ pot+" number of pan.")
+}
+cookMaagi(2,4,1)
